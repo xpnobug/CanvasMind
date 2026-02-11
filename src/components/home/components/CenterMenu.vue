7788ec5803581e96868339c0dd30800bd3a8487a
caoxupei <caoxupei@jianyou.com>
2026-02-11 22:06:48 +0800

    feat(菜单): 动态高亮当前路由菜单项

--- src/components/home/components/CenterMenu.vue
+++ src/components/home/components/CenterMenu.vue
@@ -2,18 +2,18 @@
   <div role="menu" class="lv-menu lv-menu-light lv-menu-vertical centerMenu-TCnKp8">
     <div class="lv-menu-inner">
       <!-- 灵感 -->
-      <div tabindex="0" role="menuitem" class="lv-menu-item lv-menu-selected lv-menu-item-size-default" id="Home" @click="navigateTo('/')">
+      <div tabindex="0" role="menuitem" :class="['lv-menu-item', 'lv-menu-item-size-default', { 'lv-menu-selected': currentPath === '/' }]" id="Home" @click="navigateTo('/')">
         <div class="icon-container-XAovJs" style="--menu-icon-size:48px">
-          <div class="content-XAjJup active-E3Q3lq">
-            <div class="icon-menu-m2Y0RB active-aFuBWS">
-              <div class="icon-wrap-tBuhBU">
+          <div :class="['content-XAjJup', { 'active-E3Q3lq': currentPath === '/' }]">
+            <div :class="['icon-menu-m2Y0RB', { 'active-aFuBWS': currentPath === '/' }]">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath !== '/' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M9.343 2.524a4 4 0 0 1 5.17-.122l.144.122 6 5.333A4.002 4.002 0 0 1 22 10.847V19.5a3 3 0 0 1-3 3h-3.5a2 2 0 0 1-2-2v-3a1.5 1.5 0 0 0-3 0v3a2 2 0 0 1-2 2H5a3 3 0 0 1-3-3v-8.653c0-1.07.43-2.094 1.187-2.843l.156-.147 6-5.333Z" fill="currentColor"></path>
                   </g>
                 </svg>
               </div>
-              <div class="icon-wrap-tBuhBU hide-itzP3D sf-hidden">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath === '/' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M12.166 1.52a3.991 3.991 0 0 1 2.347.882l.144.122 6 5.333A4.001 4.001 0 0 1 22 10.847V19.5a3 3 0 0 1-3 3h-5a2.98 2.98 0 0 1-2-.769 2.985 2.985 0 0 1-2 .769H5a3 3 0 0 1-3-3v-8.652c0-1.143.489-2.232 1.343-2.99l6-5.334.144-.122a4.025 4.025 0 0 1 1.473-.75A3.991 3.991 0 0 1 12 1.517c.055 0 .11.001.166.004Zm-.328 2.001c-.42.035-.831.2-1.167.499l-6 5.333c-.427.38-.671.924-.671 1.495V19.5a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V17a1 1 0 0 1 1-1l.102.005A1 1 0 0 1 13 17v2.5a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-8.652a2 2 0 0 0-.67-1.495l-6-5.333-.139-.113a1.995 1.995 0 0 0-1.03-.386l-.083-.005a1.923 1.923 0 0 0-.24.005Z" fill="currentColor"></path>
@@ -27,19 +27,19 @@
       </div>
 
       <!-- 生成 -->
-      <div tabindex="0" role="menuitem" class="lv-menu-item lv-menu-item-size-default" id="AIGeneratedRecord" @click="navigateTo('/generate')">
+      <div tabindex="0" role="menuitem" :class="['lv-menu-item', 'lv-menu-item-size-default', { 'lv-menu-selected': currentPath === '/generate' }]" id="AIGeneratedRecord" @click="navigateTo('/generate')">
         <div class="icon-container-XAovJs" style="--menu-icon-size:48px">
           <div class="container-juKD6_">
-            <div class="content-XAjJup">
-              <div class="icon-menu-m2Y0RB">
-                <div class="icon-wrap-tBuhBU hide-itzP3D sf-hidden">
+            <div :class="['content-XAjJup', { 'active-E3Q3lq': currentPath === '/generate' }]">
+              <div :class="['icon-menu-m2Y0RB', { 'active-aFuBWS': currentPath === '/generate' }]">
+                <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath !== '/generate' }]">
                   <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                     <g>
                       <path data-follow-fill="currentColor" d="M10.662 2.262c.556-1.095 2.12-1.095 2.676 0l.945 1.864a12.727 12.727 0 0 0 5.591 5.59l1.864.946c1.095.556 1.095 2.12 0 2.676l-1.864.945a12.727 12.727 0 0 0-5.59 5.591l-.946 1.864c-.556 1.095-2.12 1.095-2.676 0l-.945-1.864a12.727 12.727 0 0 0-5.591-5.59l-1.864-.946c-1.095-.556-1.095-2.12 0-2.676l1.864-.945a12.727 12.727 0 0 0 5.59-5.591l.946-1.864Z" fill="currentColor"></path>
                     </g>
                   </svg>
                 </div>
-                <div class="icon-wrap-tBuhBU">
+                <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath === '/generate' }]">
                   <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                     <g>
                       <path data-follow-fill="currentColor" d="m12 4.046-.5.985a14.727 14.727 0 0 1-6.47 6.47L4.047 12l.985.5a14.728 14.728 0 0 1 6.47 6.47l.5.984.499-.985a14.727 14.727 0 0 1 6.47-6.47l.984-.499-.985-.5a14.727 14.727 0 0 1-6.47-6.47l-.5-.983Zm1.338-1.784c-.556-1.095-2.12-1.095-2.676 0l-.945 1.864a12.727 12.727 0 0 1-5.591 5.59l-1.864.946c-1.095.556-1.095 2.12 0 2.676l1.864.945a12.727 12.727 0 0 1 5.59 5.591l.946 1.864c.556 1.095 2.12 1.095 2.676 0l.945-1.864a12.728 12.728 0 0 1 5.591-5.59l1.864-.946c1.095-.556 1.095-2.12 0-2.676l-1.864-.945a12.728 12.728 0 0 1-5.59-5.591l-.946-1.864Z" clip-rule="evenodd" fill-rule="evenodd" fill="currentColor"></path>
@@ -55,18 +55,18 @@
       </div>
 
       <!-- 资产 -->
-      <div tabindex="0" role="menuitem" class="lv-menu-item lv-menu-item-size-default" id="Asset" @click="navigateTo('/asset')">
+      <div tabindex="0" role="menuitem" :class="['lv-menu-item', 'lv-menu-item-size-default', { 'lv-menu-selected': currentPath === '/asset' }]" id="Asset" @click="navigateTo('/asset')">
         <div class="icon-container-XAovJs" style="--menu-icon-size:48px">
-          <div class="content-XAjJup">
-            <div class="icon-menu-m2Y0RB">
-              <div class="icon-wrap-tBuhBU hide-itzP3D sf-hidden">
+          <div :class="['content-XAjJup', { 'active-E3Q3lq': currentPath === '/asset' }]">
+            <div :class="['icon-menu-m2Y0RB', { 'active-aFuBWS': currentPath === '/asset' }]">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath !== '/asset' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M2.837 5.77v10.884a4 4 0 0 0 4 4h10.326a4 4 0 0 0 4-4v-8.5a3 3 0 0 0-3-3h-5.737a1 1 0 0 1-.721-.306l-1.11-1.156A3 3 0 0 0 8.43 2.77H5.837a3 3 0 0 0-3 3ZM6 10a1 1 0 1 0 0 2h12a1 1 0 1 0 0-2H6Z" clip-rule="evenodd" fill-rule="evenodd" fill="currentColor"></path>
                   </g>
                 </svg>
               </div>
-              <div class="icon-wrap-tBuhBU">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath === '/asset' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M4.837 9.65V5.77a1 1 0 0 1 1-1h2.594a1 1 0 0 1 .721.308l1.443-1.386-1.443 1.385 1.11 1.156a3 3 0 0 0 2.164.922h5.737a1 1 0 0 1 1 1V9.65H4.837Zm0 2v5.004a2 2 0 0 0 2 2h10.326a2 2 0 0 0 2-2V11.65H4.837Zm6.868-6.802-1.11-1.156A3 3 0 0 0 8.43 2.77H5.837a3 3 0 0 0-3 3v10.884a4 4 0 0 0 4 4h10.326a4 4 0 0 0 4-4v-8.5a3 3 0 0 0-3-3h-5.737a1 1 0 0 1-.721-.306Z" clip-rule="evenodd" fill-rule="evenodd" fill="currentColor"></path>
@@ -80,18 +80,18 @@
       </div>
 
       <!-- 画布 -->
-      <div tabindex="0" role="menuitem" class="lv-menu-item lv-menu-item-size-default" id="CanvasAssets" @click="navigateTo('/canvas')">
+      <div tabindex="0" role="menuitem" :class="['lv-menu-item', 'lv-menu-item-size-default', { 'lv-menu-selected': currentPath === '/canvas' }]" id="CanvasAssets" @click="navigateTo('/canvas')">
         <div class="icon-container-XAovJs" style="--menu-icon-size:48px">
-          <div class="content-XAjJup">
-            <div class="icon-menu-m2Y0RB">
-              <div class="icon-wrap-tBuhBU hide-itzP3D sf-hidden">
+          <div :class="['content-XAjJup', { 'active-E3Q3lq': currentPath === '/canvas' }]">
+            <div :class="['icon-menu-m2Y0RB', { 'active-aFuBWS': currentPath === '/canvas' }]">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath !== '/canvas' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M17.063 2.875c.69 0 1.25.56 1.25 1.25v1.563h1.562a1.25 1.25 0 1 1 0 2.5h-1.563v1.57a1 1 0 0 1-.293.708l-7.553 7.554a1 1 0 0 1-.707.293H8.187v1.562a1.25 1.25 0 1 1-2.5 0v-1.563H4.125a1.25 1.25 0 1 1 0-2.5h1.563V8.189H4.125a1.25 1.25 0 1 1 0-2.5h1.563V4.125a1.25 1.25 0 1 1 2.5 0v1.563h7.624V4.125c0-.69.56-1.25 1.25-1.25Zm-.397 11.395a.427.427 0 0 1 .794 0l.213.515a3.683 3.683 0 0 0 1.898 1.962l.605.269a.448.448 0 0 1 0 .812l-.64.284a3.68 3.68 0 0 0-1.872 1.899l-.208.477a.427.427 0 0 1-.787 0l-.208-.477a3.68 3.68 0 0 0-1.871-1.899l-.64-.284a.448.448 0 0 1 0-.812l.604-.269a3.683 3.683 0 0 0 1.898-1.962l.214-.515Z" fill="currentColor"></path>
                   </g>
                 </svg>
               </div>
-              <div class="icon-wrap-tBuhBU">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath === '/canvas' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M6.938 3.125a1 1 0 0 1 1 1v1.813h8.125V4.125a1 1 0 1 1 2 0v1.813h1.812a1 1 0 1 1 0 2h-1.813v2.937a1 1 0 1 1-2 0V7.937H7.938v8.125h2.938a1 1 0 1 1 0 2H7.937v1.813a1 1 0 1 1-2 0v-1.813H4.125a1 1 0 1 1 0-2h1.813V7.938H4.125a1 1 0 0 1 0-2h1.813V4.125a1 1 0 0 1 1-1Zm10.518 17.363.208-.477a3.68 3.68 0 0 1 1.871-1.898l.641-.285a.447.447 0 0 0 0-.812l-.605-.27a3.682 3.682 0 0 1-1.898-1.961l-.213-.515a.427.427 0 0 0-.794 0l-.214.515a3.682 3.682 0 0 1-1.898 1.962l-.605.269a.447.447 0 0 0 0 .812l.64.285a3.675 3.675 0 0 1 1.872 1.898l.208.477c.152.35.635.35.787 0Z" clip-rule="evenodd" fill-rule="evenodd" fill="currentColor"></path>
@@ -105,18 +105,18 @@
       </div>
 
       <!-- 工作流 -->
-      <div tabindex="0" role="menuitem" class="lv-menu-item lv-menu-item-size-default" id="Workflow" @click="navigateTo('/workflow')">
+      <div tabindex="0" role="menuitem" :class="['lv-menu-item', 'lv-menu-item-size-default', { 'lv-menu-selected': currentPath === '/workflow' }]" id="Workflow" @click="navigateTo('/workflow')">
         <div class="icon-container-XAovJs" style="--menu-icon-size:48px">
-          <div class="content-XAjJup">
-            <div class="icon-menu-m2Y0RB">
-              <div class="icon-wrap-tBuhBU hide-itzP3D sf-hidden">
+          <div :class="['content-XAjJup', { 'active-E3Q3lq': currentPath === '/workflow' }]">
+            <div :class="['icon-menu-m2Y0RB', { 'active-aFuBWS': currentPath === '/workflow' }]">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath !== '/workflow' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M4 5a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H9v2h6v-2h-.5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2H18a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-.5v2.5a1.5 1.5 0 0 1-1.5 1.5H9v2h.5a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h.5V14H9v-2H6.5A1.5 1.5 0 0 1 5 10.5V10h-.5A2 2 0 0 1 2.5 8V5Z" fill="currentColor"></path>
                   </g>
                 </svg>
               </div>
-              <div class="icon-wrap-tBuhBU">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath === '/workflow' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M6 5h3v3H6V5ZM4 5a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H9v2h6v-2h-.5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2H18a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-.5v2.5a1.5 1.5 0 0 1-1.5 1.5H9v2h.5a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h.5V14H9v-2H6.5A1.5 1.5 0 0 1 5 10.5V10H4.5A2 2 0 0 1 2.5 8V5Zm10.5 0h3v3h-3V5ZM6 16h3.5v3H6v-3Z" clip-rule="evenodd" fill-rule="evenodd" fill="currentColor"></path>
@@ -130,18 +130,18 @@
       </div>
 
       <!-- 账号管理 -->
-      <div tabindex="0" role="menuitem" class="lv-menu-item lv-menu-item-size-default" id="AccountManagement" @click="navigateTo('/account')">
+      <div tabindex="0" role="menuitem" :class="['lv-menu-item', 'lv-menu-item-size-default', { 'lv-menu-selected': currentPath === '/account' }]" id="AccountManagement" @click="navigateTo('/account')">
         <div class="icon-container-XAovJs" style="--menu-icon-size:48px">
-          <div class="content-XAjJup">
-            <div class="icon-menu-m2Y0RB">
-              <div class="icon-wrap-tBuhBU hide-itzP3D sf-hidden">
+          <div :class="['content-XAjJup', { 'active-E3Q3lq': currentPath === '/account' }]">
+            <div :class="['icon-menu-m2Y0RB', { 'active-aFuBWS': currentPath === '/account' }]">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath !== '/account' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M12 2a5 5 0 1 0 0 10 5 5 0 0 0 0-10ZM4 18a8 8 0 1 1 16 0 2 2 0 0 1-2 2H6a2 2 0 0 1-2-2Z" fill="currentColor"></path>
                   </g>
                 </svg>
               </div>
-              <div class="icon-wrap-tBuhBU">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath === '/account' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M12 4a3 3 0 1 0 0 6 3 3 0 0 0 0-6ZM7 7a5 5 0 1 1 10 0A5 5 0 0 1 7 7Zm-1 11a6 6 0 0 1 12 0v.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 6 18.5V18Zm6-8a8 8 0 0 0-8 8v.5A3.5 3.5 0 0 0 7.5 22h9a3.5 3.5 0 0 0 3.5-3.5V18a8 8 0 0 0-8-8Z" clip-rule="evenodd" fill-rule="evenodd" fill="currentColor"></path>
@@ -155,18 +155,18 @@
       </div>
 
       <!-- 发布中心 -->
-      <div tabindex="0" role="menuitem" class="lv-menu-item lv-menu-item-size-default" id="PublishCenter" @click="navigateTo('/publish')">
+      <div tabindex="0" role="menuitem" :class="['lv-menu-item', 'lv-menu-item-size-default', { 'lv-menu-selected': currentPath === '/publish' }]" id="PublishCenter" @click="navigateTo('/publish')">
         <div class="icon-container-XAovJs" style="--menu-icon-size:48px">
-          <div class="content-XAjJup">
-            <div class="icon-menu-m2Y0RB">
-              <div class="icon-wrap-tBuhBU hide-itzP3D sf-hidden">
+          <div :class="['content-XAjJup', { 'active-E3Q3lq': currentPath === '/publish' }]">
+            <div :class="['icon-menu-m2Y0RB', { 'active-aFuBWS': currentPath === '/publish' }]">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath !== '/publish' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M12 2a1 1 0 0 1 1 1v10.586l3.293-3.293a1 1 0 0 1 1.414 1.414l-5 5a1 1 0 0 1-1.414 0l-5-5a1 1 0 1 1 1.414-1.414L11 13.586V3a1 1 0 0 1 1-1ZM4 14a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-4a1 1 0 1 1 2 0v4a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-4a1 1 0 0 1 1-1Z" fill="currentColor"></path>
                   </g>
                 </svg>
               </div>
-              <div class="icon-wrap-tBuhBU">
+              <div :class="['icon-wrap-tBuhBU', { 'hide-itzP3D sf-hidden': currentPath === '/publish' }]">
                 <svg width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" role="presentation" xmlns="http://www.w3.org/2000/svg">
                   <g>
                     <path data-follow-fill="currentColor" d="M12 2a1 1 0 0 1 1 1v10.586l3.293-3.293a1 1 0 0 1 1.414 1.414l-5 5a1 1 0 0 1-1.414 0l-5-5a1 1 0 1 1 1.414-1.414L11 13.586V3a1 1 0 0 1 1-1ZM4 14a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-4a1 1 0 1 1 2 0v4a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-4a1 1 0 0 1 1-1Z" clip-rule="evenodd" fill-rule="evenodd" fill="currentColor"></path>
@@ -183,9 +183,13 @@
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router'
+import { computed } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 
 const router = useRouter()
+const route = useRoute()
+
+const currentPath = computed(() => route.path)
 
 const navigateTo = (path) => {
   router.push(path)
